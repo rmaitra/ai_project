0bb086541b69b2386077ba69393fe3fa90b81c9b
Raj <raj.d.maitra@gmail.com>
2017-03-24 10:25:19 -0700

    update

--- raj_ai.py
+++ raj_ai.py
@@ -2,17 +2,41 @@ import time
 import sys
 import os
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# this goes question : response
+script = {
+    "What is the derivative of arcsin(x)?":"What do YOU think the derivative of arcsin(x) is?",
+    "...I don't know.":"Well you wrote me, so how the hell do you expect me to know?"
+}
+
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
+
 
 def welcome_message():
-    print "loading Raj's A.I.",
+    print ""
+    print ""
+    print bcolors.OKBLUE + "loading N.A.S.I.R." + bcolors.ENDC,
     print_ellipse(10)
     print ""
     print ""
-    print "Raj's A.I. > Raj Arificial Intelligence (A.I.) at your service!"
-    print "Raj's A.I. > Please ask me to do something!"
+    print bcolors.WARNING + "Sup?" + bcolors.ENDC
 
 def exit_message():
-    print "Raj's A.I. > I'm glad to be of service. Goodbye!"
+    print "Adios."
+    print_thinking(4)
+    os.system('clear')
 
 
 def print_ellipse(number):
@@ -22,34 +46,34 @@ def print_ellipse(number):
         sys.stdout.flush()
 
 def print_thinking(number):
-    print "Raj's A.I. > One sec, I'm thinking",
-    for i in range(0,number):
-        time.sleep(0.2)
-        print ".",
+    spinner = spinning_cursor()
+    for _ in range(number):
+        sys.stdout.write(spinner.next())
         sys.stdout.flush()
+        time.sleep(0.1)
+        sys.stdout.write('\b')
 
 def print_ai_response(text):
     print ""
-    print ""
-    print text
+    print bcolors.WARNING + text + bcolors.ENDC
 
 def verify_response(question):
-    print ""
-    print ""
     if(question == 'exit'):
         exit_message()
 
-    #if(response == 'exit')
-    #    print_ai_response()
+    if(question in script.keys()):
+        print_thinking(15)
+        sys.stdout.write('\b')
+        print_ai_response(script[question])
+        input_command()
 
     else:
-        print_thinking(4)
-        print "I did not understand, please ask again."
+        print_thinking(15)
+        sys.stdout.write('\b')
+        print bcolors.WARNING + "Um...I dunno." + bcolors.ENDC
         input_command()
 
 def input_command():
-    print ""
-    print ""
     question = raw_input('> ')
     verify_response(question)
 
@@ -60,3 +84,5 @@ if __name__ == "__main__":
     os.system('clear')
     welcome_message()
     main()
+
+
